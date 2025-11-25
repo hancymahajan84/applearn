b07e48c1ee7ae4be1d5fa0c4355b81f084481ca6
hancymahajan84 <hancymahajan84@gmail.com>
2025-11-25 15:46:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('learning streamlit programing')
+a=st.text_input('enter your name')
+mm=st.number_input('enter your ip marks')
+if st.button('submit'):
+     st.write(f'hello! {a} , your ip marks is {mm}')
