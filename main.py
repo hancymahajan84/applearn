9f6247d1bc79216026a5cf93feddb1aa64fa58fb
hancymahajan84 <hancymahajan84@gmail.com>
2025-11-26 17:43:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+df=pd.read_csv('data.csv')
 st.title('learning streamlit programing')
 a=st.text_input('enter your name')
 mm=st.number_input('enter your ip marks')
 if st.button('submit'):
      st.write(f'hello! {a} , your ip marks is {mm}')
+     st.dataframe(df)
+     
